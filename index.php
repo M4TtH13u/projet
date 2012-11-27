72bd649cfb61aa3aa2cdf808e875d01ab2be0d09
Matthieu <kowaleswski.matthieu@eisti.eu>
2012-11-27 20:25:38 +0100

    page d'accueil

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 include'include/debut.php';
 ?>
-<form id="" action="accueil.php" method="POST">
+<form id="" action="login.php" method="POST">
 <label>Login : </label><input type="text" name="pseudo"/><br/>
-<label>Mot de passe : </label><input type="text" name="mdp"/><br/>
+<label>Mot de passe : </label><input type="password" name="mdp"/><br/>
 <input type="submit" Value="Se connecter"/>
 </form>
 <?php
-include'include/fin.php';
+
+include 'include/fin.php';
