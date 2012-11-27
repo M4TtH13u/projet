b4cdfd1a75338bb681ffa3de8f8cbbff486bb592
Matthieu <kowaleswski.matthieu@eisti.eu>
2012-11-27 20:52:11 +0100

    accueil

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
 include'include/debut.php';
+if (empty($_SESSION['idUtil']))
+{
 ?>
-<form id="" action="include/login.php" method="POST">
+<form id="cnx" action="include/login.php" method="POST">
 <label>Login : </label><input type="text" name="pseudo"/><br/>
 <label>Mot de passe : </label><input type="password" name="mdp"/><br/>
 <input type="submit" Value="Se connecter"/>
 </form>
 <?php
+}
+else
+{
+   echo'<div id="cnx">'.$_SESSION['nom'].' '.$_SESSION['prenom'].'<br/> <a href="include/logout.php">Se déconnecter</a></div>';
 
+}
 include 'include/fin.php';
