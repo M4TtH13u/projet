8603424b5cd187c50c9441659d20a7934e78e4a8
Matthieu <kowaleswski.matthieu@eisti.eu>
2012-11-27 16:35:49 +0100

    j'ai créé la page

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include'include/debut.php';
+?>
+<form id="" action="accueil.php" method="POST">
+<label>Login : </label><input type="text" name="pseudo"/><br/>
+<label>Mot de passe : </label><input type="text" name="mdp"/><br/>
+<input type="submit" Value="Se connecter"/>
+</form>
+<?php
+include'include/fin.php';
