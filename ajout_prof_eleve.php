9ff1938ff8a3610cc8a6601686dac6e70a59c69e
Matthieu <kowaleswski.matthieu@eisti.eu>
2012-12-02 20:33:33 +0100

    ready

--- ajout_prof_eleve.php
+++ ajout_prof_eleve.php
@@ -1,10 +1,49 @@
 <?php
 include'include/debut.php';
 include'include/fonctions.php';
-if (( !empty($_POST['choixUtil']) && !empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['login']) && !empty($_POST['pass']) && !empty($_POST['repass']))&&($_POST['pass']==$_POST['repass'])&&( ($_POST['choixUtil']="4")||(($_POST['choixUtil']="2")&&!empty($_POST['promo']))||(($_POST['choixUtil']="3")&&!empty($_POST['tel'])&& !empty($_POST['numBureau']))))
+if (( !empty($_POST['choixUtil']) && !empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['login']) && !empty($_POST['pass']) && !empty($_POST['repass']))&&($_POST['pass']==$_POST['repass'])&&( ($_POST['choixUtil']=="4")||(($_POST['choixUtil']=="2")&&!empty($_POST['promo']))||(($_POST['choixUtil']=="3")&&!empty($_POST['tel'])&& !empty($_POST['numBureau']))))
 {
-    echo'Ca reste a faire';
-}
+    if ($_POST['choixUtil']=="4")
+    {
+        $cnx=connect();
+        mysql_query("SET NAMES UTF8");
+        $req='INSERT INTO utilisateur VALUE("","'.$_POST['login'].'","'.$_POST['pass'].'","'.$_POST['nom'].'","'.$_POST['prenom'].'",4)';
+        $res=execReq($req);
+        deconnect($cnx); 
+        echo'L\administrateur a été ajouté';
+    }
+    elseif ($_POST['choixUtil']=="3") 
+    {
+        $cnx=connect();
+        mysql_query("SET NAMES UTF8");
+        $req='INSERT INTO utilisateur VALUE("","'.$_POST['login'].'","'.$_POST['pass'].'","'.$_POST['nom'].'","'.$_POST['prenom'].'",3)';
+        $res=execReq($req);
+        $res = execReq('select idUtil from utilisateur where login="'.$_POST['login'].'" and pass="'.$_POST['pass'].'"');
+        while($util = mysql_fetch_array($res))
+        {
+        $req2='INSERT INTO prof VALUE("","'.$_POST['numBureau'].'","'.$_POST['tel'].'","'.$util['idUtil'].'")';
+        $res2=execReq($req2);
+        }
+        deconnect($cnx);
+        echo'Le professeur a été ajouté';
+    }
+    elseif ($_POST['choixUtil']=="2")
+    {
+        $cnx=connect();
+        mysql_query("SET NAMES UTF8");
+        $req='INSERT INTO utilisateur VALUE("","'.$_POST['login'].'","'.$_POST['pass'].'","'.$_POST['nom'].'","'.$_POST['prenom'].'",2)';
+        $res=execReq($req);
+        $res = execReq('select idUtil from utilisateur where login="'.$_POST['login'].'" and pass="'.$_POST['pass'].'"');
+        while($util = mysql_fetch_array($res))
+        {
+        $req2='INSERT INTO eleve VALUE("",0,"'.$_POST['promo'].'","'.$util['idUtil'].'")';
+        $res2=execReq($req2);
+        }
+        deconnect($cnx);
+        echo'L\'élève a été ajouté';
+    }
+   
+}   
 else
 {
 ?>
